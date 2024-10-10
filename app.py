8022e2f0a926477c546a2c826442f9a94d5c5a96
Citing <162539674+Citing@users.noreply.github.com>
2024-10-10 06:14:52 +0000

    update requirements

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from utils.ai import AIAgent
 from utils.file_load import FileLoader
